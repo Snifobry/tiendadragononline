9a3365557a36331dab9813a23c4a31907049aadc
Snifobry <brycardsnifo@gmail.com>
2024-08-06 11:44:59 -0500

    Agregamos validacion con le metodo @Post en el controlador registrar con el end point /registrar

--- src/main/java/com/tiendadragon/controller/RegistrarController.java
+++ src/main/java/com/tiendadragon/controller/RegistrarController.java
@@ -1,13 +1,17 @@
 package com.tiendadragon.controller;
 
 import com.tiendadragon.entidad.Usuario;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 
-public class UsuarioController {
+public class RegistrarController {
     @GetMapping("/usuarios")
        //El objeto Model ui. hace q accedamos a los datos de la vista que en este caso datos del formulario
         public String formularioUsuario(Model model) {
@@ -17,6 +21,16 @@ public class UsuarioController {
             return "/RegistroUsuario/registroUsuario";
     }
 
+    @PostMapping("/registrar")
+    public String registrarUsuario(@Valid @ModelAttribute Usuario usuario, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            //sirve para poner errores en el formulario
+            model.addAttribute("errors", bindingResult.getAllErrors());
+            return "/RegistroUsuario/registroUsuarioError";
+        } else {
+            return "/RegistroUsuario/registroUsuarioSuccessfull";
+        }
+    }
 
 
 }
