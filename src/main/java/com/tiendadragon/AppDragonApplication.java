a0f88a506744c923b746f972c91ac9e5b0d19d22
Snifobry <brycardsnifo@gmail.com>
2024-08-05 20:06:40 -0500

    Proyecto Inicial TIenda Dragon web spring boot maven and dependencias

---
+++ src/main/java/com/tiendadragon/AppDragonApplication.java
@@ -0,0 +1,13 @@
+package com.tiendadragon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDragonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDragonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendadragon/AppDragonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendadragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDragonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
