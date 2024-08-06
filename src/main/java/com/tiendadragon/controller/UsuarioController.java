8ab3a7711de4adfd43c0498c17351a927bf439d2
Snifobry <brycardsnifo@gmail.com>
2024-08-06 01:29:48 -0500

    Aclaracion diferencia entre registor y login creacion de contrlador para registrar usuarios UsuarioCOntroller

--- src/main/java/com/tiendadragon/controller/UsuarioController.java
+++ src/main/java/com/tiendadragon/controller/UsuarioController.java
@@ -8,13 +8,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class UsuarioController {
-    @GetMapping("/usuario")
+    @GetMapping("/usuarios")
        //El objeto Model ui. hace q accedamos a los datos de la vista que en este caso datos del formulario
         public String formularioUsuario(Model model) {
             //Este "usuario" es mi opbjeto o bien dicho el model de mi objeto con sus
             //propiedades y el new es para instanciar un nuevo Usuario con el metodo new Usuario()
             model.addAttribute("usuario", new Usuario());
-            return "/Login/login";
+            return "/RegistroUsuario/registroUsuario";
     }
+
+
 
 }
